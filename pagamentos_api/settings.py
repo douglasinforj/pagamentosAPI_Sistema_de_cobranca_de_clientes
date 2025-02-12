2916d8fe9aace62095f2b2924ca03ab9b116fb2f
Douglas Silva <douglasitpro@gmail.com>
2025-02-12 18:54:27 -0300

    feat: configurando conexão banco de dados mysql com variaveis de ambiente

--- pagamentos_api/settings.py
+++ pagamentos_api/settings.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -63,12 +67,25 @@ WSGI_APPLICATION = 'pagamentos_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'pagamentos_db'),
+        'USER': os.getenv('DB_USER', 'seu_usuario'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'sua_senha'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+    }
+}
+
 
 
 # Password validation
