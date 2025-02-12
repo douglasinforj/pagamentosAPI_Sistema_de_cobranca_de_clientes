a4f5d79c5e3860bdc774270a6b407d6de672763e
Douglas Silva <douglasitpro@gmail.com>
2025-02-12 19:22:18 -0300

    feat: criando models Cliente, Produto, Venda, VendaProduto

--- pagamentos/models.py
+++ pagamentos/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255)
+    cpf_cnpj = models.CharField(max_length=18, unique=True)
+    contato = models.CharField(max_length=20)
+    endereco = models.TextField()
+
+    def __str__(self):
+        return self.nome
+    
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    valor_compra = models.DecimalField(max_digits=10, decimal_places=2)
+    valor_venda  = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
+
+class  Venda(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    produto = models.ManyToManyField(Produto, through='VendaProduto')
+    data_compra = models.DateField(auto_now_add=True)
+    data_pagamento = models.DateField()
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'Vendas {self.id} - {self.cliente.nome}'
+    
+class VendaProduto(models.Model):
+    venda = models.ForeignKey(Venda, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField()
+    preco_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.produto.nome} - {self.quantidade}x'
