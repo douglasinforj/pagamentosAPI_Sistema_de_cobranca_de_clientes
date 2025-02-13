04593f257d9286accb259ab237bdae56df5405fb
Douglas Silva <douglasitpro@gmail.com>
2025-02-13 15:00:44 -0300

    feat: configurando Rotas de Autenticação

--- pagamentos_api/urls.py
+++ pagamentos_api/urls.py
@@ -2,7 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pagamentos.urls'))
+    path('', include('pagamentos.urls')),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Gera token
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Renova token
+
 ]
