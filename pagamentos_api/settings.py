f35862f591d59174d3c86dd9ef9e62f1c998046e
Douglas Silva <douglasitpro@gmail.com>
2025-02-13 14:59:12 -0300

    feat: configurando JWT para autenticação de usuário

--- pagamentos_api/settings.py
+++ pagamentos_api/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'pagamentos',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',  # Exige autenticação para todos os endpoints
+    ),
+}
