d88cffb89a355303ba0a9b3d76464044400f6169
Douglas Silva <douglasitpro@gmail.com>
2025-02-13 10:06:45 -0300

    feat: criando viewSet

--- pagamentos/views.py
+++ pagamentos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ClienteSerializer, ProdutoSerializer, VendaSerializer
+from .models import Cliente, Produto, Venda
 
-# Create your views here.
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+class VendaViewSet(viewsets.ModelViewSet):
+    queryset = Venda.objects.all()
+    serializer_class = VendaSerializer
