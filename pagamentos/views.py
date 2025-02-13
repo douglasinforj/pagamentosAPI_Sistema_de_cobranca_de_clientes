dacb5ab07e07b571d99774a2ae258a5f10ee0d55
Douglas Silva <douglasitpro@gmail.com>
2025-02-13 11:48:01 -0300

    feat: Criando Filtros para Cliente e Vendas por Clientes e Data

--- pagamentos/views.py
+++ pagamentos/views.py
@@ -3,10 +3,20 @@ from rest_framework import viewsets
 from .serializers import ClienteSerializer, ProdutoSerializer, VendaSerializer
 from .models import Cliente, Produto, Venda
 
+#filtros:
+from rest_framework import filters
+from django.utils.timezone import now
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Q
+
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
+    #filtro clientes
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nome', 'cpf_cnpj']
 
 class ProdutoViewSet(viewsets.ModelViewSet):
     queryset = Produto.objects.all()
@@ -15,3 +25,20 @@ class ProdutoViewSet(viewsets.ModelViewSet):
 class VendaViewSet(viewsets.ModelViewSet):
     queryset = Venda.objects.all()
     serializer_class = VendaSerializer
+
+    #filtrando vendas por cliente e data
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['cliente_nome']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        cliente_id = self.request.query_params.get('cliente')
+        data_inicio = self.request.query_params.get('data_inicio')
+        data_fim = self.request.query_params.get('data_fim')
+
+        if cliente_id:
+            queryset = queryset.filter(cliente_id=cliente_id)
+        if data_inicio and data_fim:
+            queryset = queryset.filter(data_pagamento__range=[data_inicio, data_fim])
+            
+        return queryset
