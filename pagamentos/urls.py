2eac29d9e7a73a61585c7f4fd24c278edca921ba
Douglas Silva <douglasitpro@gmail.com>
2025-02-13 19:41:48 -0300

    feat: Adicionando rotas especificas com JWT

--- pagamentos/urls.py
+++ pagamentos/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ClienteViewSet, ProdutoViewSet, VendaViewSet
-from .views import AuthViewSet
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 router = DefaultRouter()
 router.register(r'clientes', ClienteViewSet)
 router.register(r'produtos', ProdutoViewSet)
 router.register(r'vendas', VendaViewSet)
-router.register(r'auth', AuthViewSet, basename='auth')
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),   # Login
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),        # Refresh do Token
 ]
